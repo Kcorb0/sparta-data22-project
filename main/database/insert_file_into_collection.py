1057f580445de86bd659da33906aac45f1704c42
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-07-21 16:04:58 +0100

    Function created to load files to mongoDB

--- main/database/insert_file_into_collection.py
+++ main/database/insert_file_into_collection.py
@@ -8,4 +8,14 @@ def insert_file_into_collection(file_list):
         print(f'{name} has been inserted.')
         db.sparta_day.insert_one(i)
 
-insert_file_into_collection([{'name': 'Darb Noor', 'date': '04/04/2019', 'tech_self_score': {'Python': 1, 'JavaScript': 5}, 'strengths': ['Versatile', 'Determined', 'Composure'], 'weaknesses': ['Undisciplined'], 'self_development': 'Yes', 'geo_flex': 'Yes', 'financial_support_self': 'Yes', 'result': 'Pass', 'course_interest': 'Business'}])
+insert_file_into_collection([{'name': 'Darb Noor', 
+    'date': '04/04/2019', 
+    'tech_self_score': {'Python': 1, 'JavaScript': 5}, 
+    'strengths': ['Versatile', 'Determined', 'Composure'], 
+    'weaknesses': ['Undisciplined'], 
+    'self_development': 'Yes', 
+    'geo_flex': 'Yes', 
+    'financial_support_self': 'Yes', 
+    'result': 'Pass', 
+    'course_interest': 'Business'}]
+)
