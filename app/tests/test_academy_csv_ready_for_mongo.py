19aa80ecbd159849957c629fbc0b644df19835c9
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-07-28 13:19:56 +0100

    clean directory

--- app/tests/test_academy_csv_ready_for_mongo.py
+++
@@ -1,9 +0,0 @@
-from app.load.ready_for_mongodb.academy_csv_ready_for_mongo import *
-
-
-def test_get_all_academy_filepath():
-    assert type(get_all_academy_filepath()) is list
-
-
-def test_loop_through_academy_filepath():
-    assert type(loop_through_academy_filepath()) is list
