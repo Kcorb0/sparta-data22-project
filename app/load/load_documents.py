00a1551e8ed6c4fe78052742aa5380f3563e18e2
myleslangston <84974498+myleslangston@users.noreply.github.com>
2021-07-28 13:56:29 +0100

    Logging introduced for loading stage

--- app/load/load_documents.py
+++ app/load/load_documents.py
@@ -4,6 +4,17 @@ from app.load.list_talent_json_filenames import get_talent_jsons
 from app.load.list_academy_csv_filenames import get_academy_csvs
 from app.load.list_spartaday_csv_filenames import get_spartaday_csvs
 from app.load.list_applicants_csv_filenames import get_applicants_csvs
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('loading.log')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 
 def load_documents(file_list, file_type, collection):
@@ -24,7 +35,12 @@ def load_documents(file_list, file_type, collection):
 
 
 def load_to_mongodb():
+    logger.info('Cleaned files are being loaded to MongoDB')
     load_documents(get_talent_jsons(), 'json', 'talent')
+    logger.info('Talent files have been successfully loaded.')
     load_documents(get_academy_csvs(), 'csv', 'academy')
+    logger.info('Academy files have been successfully loaded.')
     load_documents(get_spartaday_csvs(), 'csv', 'spartaday')
+    logger.info('Sparta day files have been successfully loaded.')
     load_documents(get_applicants_csvs(), 'csv', 'applicants')
+    logger.info('Applicant files have been successfully loaded.')
