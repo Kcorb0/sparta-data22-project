2a70a8c1444c64eaaf45445269c834ec2a43d47b
Amy <amy.zhang7@gmail.com>
2021-07-29 10:06:49 +0100

    Get rid of adding the quote marks on phone number - Line 74

--- app/transform/clean_applicants_csv.py
+++ app/transform/clean_applicants_csv.py
@@ -71,7 +71,6 @@ def change_phone_number_multiple(df):
     remove_characters = ['-', '(', ')', ' ']
     for character in remove_characters:
         change_phone_number(df, character)
-    df['phone_number'] = ('\"' + df['phone_number'] + '\"')
 
 
 def clean_applicants_csv_file(file_path):
